c5ae579d64d13c05f4babb7e56d2f2b3c758f32d
Nicole <58612725+kitethenite@users.noreply.github.com>
2024-02-20 12:57:51 +0100

    biotope information added, with workin JS

--- js/biotop-nicole.js
+++ js/biotop-nicole.js
@@ -2,5 +2,14 @@
 const urlParams = new URLSearchParams(window.location.search);
 const tag = urlParams.get("tags");
 
-
 document.querySelector(".title").textContent = tag;
+
+// bestämmer vilken biotop-text som visas
+
+if (tag === 'city') {
+    document.querySelector('.biotope-city').classList.remove("hidden");
+} else if (tag === 'suburban') {
+    document.querySelector('.biotope-suburban').classList.remove("hidden");
+} else if (tag === 'park') {
+    document.querySelector('.biotope-park').classList.remove("hidden");
+}
